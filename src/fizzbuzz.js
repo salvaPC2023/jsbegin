7e2cc7e0dadf0ab6916f0100f73c79d18557acc4
Salvador_Patty_Camacho <salvador.patty@ucb.edu.bo>
2024-02-22 10:17:30 -0400

    Imprime una secuencia de mas numeros tomando encuenta el caso fizz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,11 @@
 function generarfizzbuzz(n) {
-    
-    return n + "" ;
+    let mensaje = "";
+    if(n % 3 == 0){
+        mensaje = "fizz";
+    }else{
+        mensaje = n + "";
+    }
+    return mensaje;
+   //return n + "" ;
   }
   export default generarfizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,8 +4,13 @@ describe("FizzBuzz", () => {
   it("Deberia ganar el mismo numero para uno que no tiene reglas por ejemplo el 1", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
   });
-  it("Deberia ganar el mismo numero para uno que no tiene reglas por ejemplo el 1", () => {
+  it("Deberia ganar el mismo numero para uno que no tiene reglas por ejemplo el 2", () => {
     expect(generarFizzBuzz(2)).toEqual("2");
   });
-
+  it("Deberia mostrar la palabra fizz para el numero 3", () => {
+    expect(generarFizzBuzz(3)).toEqual("fizz");
+  });
+  it("Deberia mostrar la palabra fizz para los multiplos del numero 3", () => {
+    expect(generarFizzBuzz(6)).toEqual("fizz");
+  });
 });
